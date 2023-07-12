f69d7caed794e7c6304152364c8bf9157e51f29c
Francesca Marshall <francescam3105@gmail.com>
2023-07-12 16:47:27 +0100

    tested getter/setter for firstName

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,2 +1,26 @@
+import java.time.LocalDate;
+
 public class BankAccount {
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private int accountNumber;
+    private int balance;
+
+    public  BankAccount(String firstName, String lastName, LocalDate dateOfBirth, int accountNumber, int balance){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String newFirstName){
+        this.firstName = newFirstName;
+    }
 }
--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,2 +1,62 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class BankAccountTest {
+    BankAccount bankAccount;
+
+    LocalDate today = LocalDate.now();
+
+    @BeforeEach
+    public void setUp(){
+        bankAccount = new BankAccount("FirstName", "LastName", today, 01234567, 15);
+    }
+
+//    ------------------------------------------
+    @Test
+    public void canGetFirstName(){
+//        Given
+//        When
+        String result = bankAccount.getFirstName();
+//        Then
+        String expected = "FirstName";
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void canSetFirstName(){
+//        When
+        bankAccount.setFirstName("newFirstName");
+        String result = bankAccount.getFirstName();
+//        Then
+        String expected = "newFirstName";
+        assertThat(result).isEqualTo(expected);
+    }
+//--------------------------------------------------
+
+    @Test
+    public void canGetFirstName(){
+//        Given
+//        When
+        String result = bankAccount.getFirstName();
+//        Then
+        String expected = "FirstName";
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void canSetFirstName(){
+//        When
+        bankAccount.setFirstName("newFirstName");
+        String result = bankAccount.getFirstName();
+//        Then
+        String expected = "newFirstName";
+        assertThat(result).isEqualTo(expected);
+    }
+
+
 }
+
